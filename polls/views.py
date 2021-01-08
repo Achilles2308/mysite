864a11770dd33b0e0092362e3bd2f98118a010b0
jsi23541 <jasper.sival@ordina.nl>
2021-01-08 14:18:29 +0100

    Shortcut get_object_or_404

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,6 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from .models import Question
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 
 def index(request):
@@ -12,7 +12,8 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse(f"You're looking at question {question_id}.")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', context={'question': question})
 
 
 def results(request, question_id):
